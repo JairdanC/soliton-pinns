7061515d73dd4024e30ec6649f0c9e584abbc818
JairdanC <jairdanchopra@gmail.com>
2026-07-14 16:03:47 -0400

    Added dimension peaks specific error

--- models/kdv/tester.py
+++ models/kdv/tester.py
@@ -1,5 +1,7 @@
 #Libraries
 import torch
+import numpy as np
+from scipy.signal import find_peaks
 #Types
 from .types import ErrorStats, TestingDomain
 
@@ -55,3 +57,72 @@ def test(u_pred: torch.Tensor,
         print(f"Maximum: {max_error:.6e}")
 
     return error_stats
+
+def compute_x_diff(u_pred: np.ndarray,
+                   u_exact: np.ndarray,
+                   n_solitons: int,
+                   x: np.ndarray
+                   ) -> np.ndarray:
+    """
+    Computes the x difference between soliton peaks, scaled to the domain
+    ----
+    Picks out the n peaks of the solitons of both the exact and predicted solutions. 
+    Takes the corresponding values in the x domain, computes the x difference
+    """
+
+    try:  
+        exact_peak_idx, _ = find_n_peaks(u_exact, num=n_solitons, distance=1)
+        pred_peak_idx, _ = find_n_peaks(u_pred, num=n_solitons, distance=1)
+
+        if exact_peak_idx.size != pred_peak_idx.size:
+            raise ValueError("Different numbers of peaks found")
+        
+    except ValueError:
+        error =  np.ones(n_solitons) * np.nan
+    else:
+        scale = x[-1] - x[0] 
+        diff = np.abs(x[exact_peak_idx] - x[pred_peak_idx]) / scale
+        error = np.ones(n_solitons) * np.nan
+        error[:diff.size] = diff
+    finally:
+        return error
+
+def compute_amp_diff(u_pred: np.ndarray,
+                     u_exact: np.ndarray,
+                     n_solitons: int
+                     ) -> np.ndarray:
+    """
+    Computes the amplitude difference between soliton peaks, scaled to the domain
+    ----
+    Picks out the n peaks of the solitons of both the exact and predicted solutions. 
+    Takes the corresponding values in the u domain, computes the amplitude difference
+    """
+
+    try:  
+        exact_peak_idx, _ = find_n_peaks(u_exact, num=n_solitons, distance=1)
+        pred_peak_idx, _ = find_n_peaks(u_pred, num=n_solitons, distance=1)
+
+        if exact_peak_idx.size != pred_peak_idx.size:
+            raise ValueError("Different numbers of peaks found")
+        
+    except ValueError:
+        error =  np.ones(n_solitons) * np.nan
+    else:
+        scale = np.max(np.abs(u_exact)) 
+        diff = np.abs(u_exact[exact_peak_idx] - u_pred[pred_peak_idx]) / scale
+        error = np.ones(n_solitons) * np.nan
+        error[:diff.size] = diff
+    finally:
+        return error
+
+def find_n_peaks(signal: np.ndarray, num: int, distance: int) -> np.ndarray:
+        "Recursive helper function to find the correct number of soliton peaks"
+        if distance > signal.size:
+            raise ValueError("Distance > signal")
+        
+        peak_idx, _ = find_peaks(signal, distance=distance)
+        if peak_idx.size > num:
+            peak_idx, _ = find_n_peaks(signal=signal, num=num, distance=distance+1)
+            return peak_idx, _
+        else:
+            return peak_idx, _
